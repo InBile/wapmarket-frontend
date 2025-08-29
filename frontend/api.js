10a4677c7140c3fa001625ddd162a09708a32985
InBile <onabile1999@gmail.com>
2025-08-29 13:37:45 +0100

    Update api.js

--- frontend/api.js
+++ frontend/api.js
@@ -1,3 +1,3 @@
 // Reemplaza con tu URL de Railway, por ejemplo:
 // window.API_BASE = 'https://wapmarket.up.railway.app/api';
-window.API_BASE = 'https://TU-BACKEND-RAILWAY.up.railway.app/api';
+window.API_BASE = 'https://wapmarket-backend-production.up.railway.app/api';
